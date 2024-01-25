528250dd6e11087ad1c63c5a9c464aa86ef21690
Jofre Aleman <jofre.aleman.7e7@itb.cat>
2024-01-25 18:43:10 +0100

    ex 3

--- e3.py
+++ e3.py
@@ -5,9 +5,23 @@ ASIXc1 MO3-UF1 Pr5
 Exercici 3:  Programa de traducció d’insults. Crear una estructura de dues dimensions amb els insults en
 català i afegir la traducció en castellà, anglès i klingon
 """
-import deep_translator
+from translate import Translator
+import langdetect
 
-traductor = deep_translator.GoogleTranslator(source='es', target='en')
-resultado = traductor.translate("La educación es el arma más poderosa para cambiar al mundo")
-print(resultado)
-
+listainsults = {
+    "CAT": ("MOCOS", "CAPSIGRANY", "CALAMAR", "CARAMULL", "CAPOLL", "CAGAMANDURRIES", "CARRINCLÓ", "CABRA", "CARAGIRAT", "COLLONS", "CASCALL", "CAGALERA", "CACAFUTI"),
+    "ESP": ("Mocoso", "Cabezon", "Calamar", "Caramull", "Capullo", "Cagamandurrias", "Carrinchón", "Cabra", "Caragirado", "Collons", "Cascarrabias", "Cagalera", "Cacafuti"),
+    "ENG": ("Snuffly", "Big", "Squid", "Blockhead", "Jerk", "Dunderhead", "Nincompoop", "Goat", "Blockhead", "Balls", "Fool", "Loon", "Noodle"),
+    "KLI": ("Bach", "p", "Q'Pla", "QI'yaH", "nIQ", "Hurgh", "Qapla'", "Saj", "QI'yaH", "bIQ", "tIQ", "lo'", "ghIl")
+}
+palabra = input()
+if palabra.upper() in listainsults["CAT"]:
+    aux = listainsults["CAT"].index(palabra.upper())
+    print(listainsults["ESP"][aux])
+    print(listainsults["ENG"][aux])
+    print(listainsults["KLI"][aux])
+else:
+    aux2 = langdetect.detect_langs(palabra)[0].split(":")
+    traduccion = Translator(to_lang="es", from_lang=aux2[0])
+    otrabyna = traduccion.translate(palabra)
+    print(otrabyna)
